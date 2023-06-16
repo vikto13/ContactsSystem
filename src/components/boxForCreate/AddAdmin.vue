17fc04a3724ecbf8dc9965b2da7b040905cefd8e
Viki <vktorija.vilimaite@teltonika.lt>
2023-06-16 08:12:20 +0300

    email changing is enabled

--- src/components/boxForCreate/AddAdmin.vue
+++ src/components/boxForCreate/AddAdmin.vue
@@ -33,7 +33,6 @@
                         :placeholder="'Įveskite el.paštą...'"
                         class="form-control table-footer"
                         :style="{ 'border-left-width': 0 }"
-                        :disabled="admin.id"
                     />
                 </input-box-icon>
 
